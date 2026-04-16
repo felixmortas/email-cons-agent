8c746ff153e36cda19991fdee01c8e955d9f8fc6
felixmortas <felix.mortas@hotmail.fr>
2026-04-16 18:47:49 -0500

    improve model context by removing message triming

--- agent/agent.py
+++ agent/agent.py
@@ -5,7 +5,6 @@ from langchain_mistralai import ChatMistralAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 from agent.tools import get_tools
-from agent.middleware.trim_messages import make_trim_messages
 from agent.middleware.model_fallback import fallback
 from agent.middleware.dynamic_page_snapshot import make_dynamic_page_snapshot
 from agent.state import State
@@ -14,11 +13,9 @@ from agent.context import Context
 def create_email_agent(system_prompt, page, model_name: str = "mistral-large-latest"):
     if model_name.startswith("gemini"):
         model = ChatGoogleGenerativeAI(model=model_name, api_key=os.getenv('GOOGLE_API_KEY')) # gemini-3-flash-preview gemini-2.5-pro gemini-2.5-flash gemini-2.5-flash-lite gemini-2.5-flash-lite-preview-09-2025 gemini-2.5-flash-native-audio-preview-12-2025 gemini-2.5-flash-preview-tts gemini-2.0-flash gemini-2.0-flash-lite
-        trim_messages = make_trim_messages(is_gemini=True)
 
     elif model_name.startswith("mistral"):
         model = ChatMistralAI(model=model_name, api_key=os.getenv('MISTRAL_API_KEY'))
-        trim_messages = make_trim_messages(is_gemini=False)
 
     else:
         raise ValueError(f"Modèle non supporté : {model_name}. Utilisez un modèle 'gemini-*' ou 'mistral-*'.")
@@ -31,7 +28,6 @@ def create_email_agent(system_prompt, page, model_name: str = "mistral-large-lat
         state_schema=State, 
         context_schema=Context, 
         middleware=[page_snapshot,
-                    trim_messages, 
                     fallback],
         system_prompt=system_prompt,
         debug=True
