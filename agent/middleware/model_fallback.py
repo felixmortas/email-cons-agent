f0495bd99e920b4a317af2da355d8f952363d77b
felixmortas <felix.mortas@hotmail.fr>
2026-04-16 15:58:46 -0500

    improve model fallback list

--- agent/middleware/model_fallback.py
+++ agent/middleware/model_fallback.py
@@ -1,8 +1,17 @@
 from langchain.agents.middleware.model_fallback import ModelFallbackMiddleware
 
+# Free models:
+# - https://ai.google.dev/gemini-api/docs/pricing?hl=fr
+
+
 fallback = ModelFallbackMiddleware(
-    "google_genai:gemini-3-flash-preview",
-    "google_genai:gemini-3.1-flash-lite-preview",
-    "mistralai:mistral-small-latest",
     "mistralai:mistral-large-latest",
+    # "google_genai:gemini-3.1-flash-live-preview",
+    "google_genai:gemini-3.1-flash-lite-preview",
+    "google_genai:gemini-3-flash-preview",
+    "google_genai:gemini-2.5-pro",
+    "mistralai:mistral-small-latest",
+    "google_genai:gemini-2.5-flash",
+    "google_genai:gemini-2.5-flash-lite",
+    "google_genai:gemini-2.5-flash-lite-preview-09-2025",
 )
