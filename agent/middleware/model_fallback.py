8e70c17f0d511af75d3bcfa1f0201efabb88a220
felixmortas <felix.mortas@hotmail.fr>
2026-04-16 11:35:51 -0500

    update model fallback to keep good models first then smaller ones

--- agent/middleware/model_fallback.py
+++ agent/middleware/model_fallback.py
@@ -1,8 +1,8 @@
 from langchain.agents.middleware.model_fallback import ModelFallbackMiddleware
 
 fallback = ModelFallbackMiddleware(
-    "mistralai:mistral-small-latest",
-    "google_genai:gemini-3.1-flash-lite-preview",
     "google_genai:gemini-3-flash-preview",
+    "google_genai:gemini-3.1-flash-lite-preview",
+    "mistralai:mistral-small-latest",
     "mistralai:mistral-large-latest",
 )
