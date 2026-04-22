f69a46e5cf4241f2c8acb42fb0b9b92ca18b805f
felixmortas <felix.mortas@hotmail.fr>
2026-04-22 01:56:41 -0500

    add capcha bypass (not working for annaconda website)

--- services/playwright_session.py
+++ services/playwright_session.py
@@ -1,13 +1,15 @@
 from playwright.async_api import async_playwright
+from playwright_stealth import Stealth
 from contextlib import asynccontextmanager
 
 from context import ContextSchema
 
 @asynccontextmanager
 async def playwright_session(context: ContextSchema, headless: bool = True):
-    async with async_playwright() as p:
+    async with Stealth().use_async(async_playwright()) as p:
         browser = await p.chromium.launch(headless=headless, slow_mo=500)
         context.page = await browser.new_page()
+
         try:
             yield context
         finally:
